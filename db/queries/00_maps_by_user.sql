19a7c6bacad71840eab9e51cd1884fb3d7746eb7
Kristy Gislason <kristygislason@gmail.com>
2023-01-04 14:30:04 -0800

    Rename maps by user and update query to look for any pins the user has created for a given map

--- db/queries/00_maps_by_user.sql
+++
@@ -1,4 +0,0 @@
-SELECT title FROM maps
-JOIN pins on maps.id = map_id
-JOIN users on users.id = user_id
-WHERE user_id = $1;
--- db/queries/maps_by_user.sql
+++ db/queries/maps_by_user.sql
@@ -1,5 +1,4 @@
--- Select all maps for a given user (author)
-SELECT *
-FROM maps
-WHERE user_id = 1
-;
+SELECT maps.title FROM maps
+JOIN pins on maps.id = map_id
+JOIN users on users.id = pins.user_id
+WHERE pins.user_id = 1;
