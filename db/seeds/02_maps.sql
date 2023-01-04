07b26643d9aa26c72b44b66c3c5025d0a8919c6e
Kristy Gislason <kristygislason@gmail.com>
2023-01-04 13:50:55 -0800

    Add correct user id so we can run this in the db without errors

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,2 +1,3 @@
 -- Maps table seeds here
-INSERT INTO maps (user_id, title, longitude, latitude, zoom) VALUES (0, 'Best Mexican Food in Metro Vancouver', '49.3225', '122.8568');
+INSERT INTO maps (user_id, title, longitude, latitude, zoom) 
+VALUES (1, 'Best Mexican Food in Metro Vancouver', '49.3225', '122.8568', '6');
