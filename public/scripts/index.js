10fecaa0f66e6dca5a73b7095a19befdccbe9ef8
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-01-06 13:05:46 -0700

    updated the map script

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 51.04862, lng: -114.07085 },
-    zoom: 15,
+    zoom: 13,
   });
 }
 
