ad0779cd0844a5e03d4dceea3306a2e1b027014b
Lauren Ashley <la.zaubi@gmail.com>
2023-01-05 19:49:39 -0800

    fixed spacing, maps routing works now

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(
