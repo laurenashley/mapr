19bba2725fe95123485cdea3004a76435ceca1a0
Lauren Ashley <la.zaubi@gmail.com>
2023-01-05 21:08:58 -0800

    added more map seeds

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,3 +1,6 @@
 -- Maps table seeds here
-INSERT INTO maps (user_id, title, longitude, latitude, zoom) 
+INSERT INTO maps (user_id, title, longitude, latitude, zoom)
 VALUES (1, 'Best Mexican Food in Metro Vancouver', '49.3225', '122.8568', '6');
+VALUES (1, 'Best Boutique Hotels in Metro Vancouver', '49.3225', '122.8568', '6');
+VALUES (0, 'Korean BBQ in Metro Vancouver', '49.3225', '122.8568', '6');
+VALUES (0, 'IMAX Theatres in Metro Vancouver', '49.3225', '122.8568', '6');
