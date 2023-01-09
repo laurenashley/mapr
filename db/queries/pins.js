a08747f01b6d80cd791fd4eb2494e02639ed7dad
Lauren Ashley <la.zaubi@gmail.com>
2023-01-08 20:39:13 -0800

    create pins query js file

---
+++ db/queries/pins.js
@@ -0,0 +1,24 @@
+/* eslint-disable arrow-body-style */
+const db = require('../connection');
+
+const getPins = (mapID) => {
+  return db.query(`SELECT * FROM pins WHERE map_id = $1;`, [mapID])
+    .then(data => {
+      return data.rows;
+    });
+};
+
+const getSinglePin = (id) => {
+  return db.query(`SELECT * FROM pins WHERE pin_id = $1;`, [id])
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = {
+  getMaps,
+  getSingleMap,
+  updateMap,
+  addNewMap,
+  deleteMap
+};
---
+++ routes/pins.js
@@ -0,0 +1,17 @@
+/*
+ * All routes for Map Data are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /api/widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+
+const router  = express.Router();
+const mapsQueries = require('../db/queries/pins');
+
+router.get('/', (req, res, mapID) => {
+  mapsQueries.getpins(mapID);
+});
+
+module.exports = router;
