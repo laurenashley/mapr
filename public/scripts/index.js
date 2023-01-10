f2b88598633bd447f3417313f1005f57901df4bd
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-01-10 11:23:52 -0700

    refractor the map index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,17 +6,8 @@ function initMap() {
     zoom: 12,
   });
 
-  // Create a <script> tag and set the USGS URL as the source.
-  const script = document.createElement("script");
-
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  pins;
-  document.getElementsByTagName("head")[0].appendChild(script);
-
 
 }
-
 
 
 window.initMap = initMap;
