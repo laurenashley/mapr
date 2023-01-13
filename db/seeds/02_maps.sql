9362e9a71d1a1b54fcb09ef778ae4f1a378a9cc4
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-01-12 19:01:34 -0700

    Made changes in seeds

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO maps (user_id, title, latitude, longitude, zoom)
 VALUES
-(1, 'Best Hiking Mountains in British Columbia', '49.2578262', '-123.1941153', '7.8'),
+(1, 'Best Hiking Trails in British Columbia', '49.2578262', '-123.1941153', '7.8'),
 (1, 'Best Mexican Food in Vancouver', '49.2578262', '-123.1941153', '11'),
 (2, 'Korean BBQ in Vancouver', '49.2576508', '-123.2639868', '11'),
 (2, 'IMAX Theatres in Vancouver', '49.2576508', '-123.2639868','11'),
