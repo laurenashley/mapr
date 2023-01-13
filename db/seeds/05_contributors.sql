e24d53a1192a49020cb056e4d2bb89164fc8bf6b
Lauren Ashley <la.zaubi@gmail.com>
2023-01-13 11:01:44 -0800

    added jayrs seeds and styling, add note for contribs table

--- db/seeds/05_contributors.sql
+++ db/seeds/05_contributors.sql
@@ -1,3 +1,6 @@
 -- Contributors table seeds here
 INSERT INTO contributors (pin_id, user_id) VALUES (1, 1);
 INSERT INTO contributors (pin_id, user_id) VALUES (2, 1);
+INSERT INTO contributors (pin_id, user_id) VALUES (5, 1);
+INSERT INTO contributors (pin_id, user_id) VALUES (4, 1);
+INSERT INTO contributors (pin_id, user_id) VALUES (3, 1);
