e8376adb3a764448e68132d79e84a5c1136a5fd4
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-01-13 00:09:22 -0700

    Added more seed files for content and added styling in the side bar

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -4,6 +4,7 @@ INSERT INTO maps (user_id, title, latitude, longitude, zoom)
 VALUES
 (1, 'Best Hiking Trails in British Columbia', '49.2578262', '-123.1941153', '7.8'),
 (1, 'Best Mexican Food in Vancouver', '49.2578262', '-123.1941153', '11'),
-(2, 'Korean BBQ in Vancouver', '49.2576508', '-123.2639868', '11'),
-(2, 'IMAX Theatres in Vancouver', '49.2576508', '-123.2639868','11'),
-(1, 'Best Hotels in Vancouver', '49.2576508', '-123.2639868', '11');
+(1, 'Best Attractions Spots in Alberta', '51.0460954', '-114.065465', '6.8'),
+(2, 'Best Tourist Spots Around Montreal', '45.5031824', '-73.5698065', '7'),
+(2, 'Best place to eat Toronto', '43.6534817', '-79.3839347','11'),
+(1, 'Best Airbnb in Niagara Falls', '43.1065603', '-79.0639039', '11');
--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,29 +1,71 @@
 -- Pins table seeds here
--- INSERT INTO pins (map_id, user_id, title, description, image_url, longitude, latitude)
--- VALUES
--- (2, 1, 'The Mexican Antojitos y Cantina', 'desc', 'https://picsum.photos/200/300', '-123.124006', '49.278797'),
--- (3, 1, 'La Taqueria Pinche Taco Shop', 'desc', 'https://picsum.photos/200/300', '-123.112674', '49.263558');
--- VALUES (1, 1, 'The Mexican Antojitos y Cantina', 'desc', 'https://picsum.photos/100/100', '-123.124006', '49.278797');
-
 INSERT INTO pins (map_id, user_id, title, description, image_url, longitude, latitude)
 VALUES
 
-(1, 1, 'Sunshine Coast Trail', 'Located in the Powell River area, Canadas longest hut-to-hut hiking trail. It stretches over 180km that range from coastal shorelines and old growth forests to pristine lakes and panoramic mountaintops.', 'https://images.pexels.com/photos/287240/pexels-photo-287240.jpeg?auto=compress&cs=tinysrgb&w=200&h=200&dpr=1', '-124.1367', '49.8114'),
+-- BC
+(1, 1, 'Sunshine Coast Trail', 'Located in the Powell River area, Canadas longest hut-to-hut hiking trail. It stretches over 180km that range from coastal shorelines and old growth forests to pristine lakes and panoramic mountaintops.', 'https://images.pexels.com/photos/287240/pexels-photo-287240.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1', '-124.1367', '49.8114'),
 
-(1, 1, 'Al Habrichs Ridge Trail', 'Taking around 5-6 hours to hike, as the walk leads you out near the majestic Neverland Lake The route goes just beyond Neverland Lake and an extended route can be hiked to Neverland Falls. The Als Habrich Ridge Trail is best done by taking the Sea To Sky gondola to the top but can also be done after hiking the Sea To Summit route from the bottom of the gondola.', 'https://images.pexels.com/photos/56760/pexels-photo-56760.jpeg?auto=compress&cs=tinysrgb&w=200&h=200&dpr=1', '-123.115239', '49.676976'),
+(1, 1, 'Al Habrichs Ridge Trail', 'Taking around 5-6 hours to hike, as the walk leads you out near the majestic Neverland Lake The route goes just beyond Neverland Lake and an extended route can be hiked to Neverland Falls. The Als Habrich Ridge Trail is best done by taking the Sea To Sky gondola to the top but can also be done after hiking the Sea To Summit route from the bottom of the gondola.', 'https://images.pexels.com/photos/56760/pexels-photo-56760.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1', '-123.115239', '49.676976'),
 
-(1, 1, 'Black Tusk Trail', 'The dark, jagged edges of Black Tusk make it one of the most scenic and unique hikes in southwestern British Columbia. Black Tusk is the most spectacular mountain in the area, and one of the most difficult terrain, with 29 km round trip. and elevation gain of 1740 meters. ', 'https://images.pexels.com/photos/868097/pexels-photo-868097.jpeg?auto=compress&cs=tinysrgb&w=200&h=200&dpr=1', '-123.042753', '49.9751162'),
+(1, 1, 'Black Tusk Trail', 'The dark, jagged edges of Black Tusk make it one of the most scenic and unique hikes in southwestern British Columbia. Black Tusk is the most spectacular mountain in the area, and one of the most difficult terrain, with 29 km round trip. and elevation gain of 1740 meters. ', 'https://images.pexels.com/photos/868097/pexels-photo-868097.jpeg?auto=compress&cs=tinysrgb&w=500&h=500&dpr=1', '-123.042753', '49.9751162'),
 
-(2, 1, 'La Taqueria Pinche Taco Shop', 'desc', 'https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=200&h=200&dpr=1', '-123.112674', '49.263558'),
+(1, 1, 'Elfin Lakes', 'Two lakes in Garibaldi Provincial Park that are popular for hiking, snowshoeing, skiing, and mountain bicycling trail located east of Squamish, British Columbia, Canada.', 'https://upload.wikimedia.org/wikipedia/commons/1/1c/ElfinPano.jpg', '-122.4762', '50.3413'),
 
-(2, 1, 'Los Amigos Taqueria', 'Small, cheery taqueria serving creative tacos, salsas & other Mexican eats in no-frills surrounds.', 'https://picsum.photos/100/100', '-123.1354019', '49.2806656'),
+(1, 1, 'Joffre Lakes', 'A provincial park located 35 kilometres (22 mi) east of Pemberton in British Columbia, Canada. It was established in 1996, when Joffre Lakes Recreation Area (created 1988) was upgraded to park status. Three glacier-fed lakes are located in the park: Lower, Middle, and Upper Joffre Lakes. The rock flour in the water reflects blue and green waves, and gives the lakes a turquoise colour. Joffre Lakes is a popular destination for hikers in the summer and mountain climbers in the winter due to its scenic lakes, glaciers and challenging peaks.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Upper_Joffre_Lake_-_British_Columbia%2C_Canada.jpg/1920px-Upper_Joffre_Lake_-_British_Columbia%2C_Canada.jpg', '-122.98824611814419', '49.787158149999996'),
 
-(2, 1, 'Cuchillo', 'Latin American small plates, creative entrees & cocktails in arty, industrial-chic surrounds.', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+-- VC
+(2, 1, 'The Mexican Antojitos y Cantina', 'The Mexican, antojitos y cantina offers many fine traditional Mexican dishes with the real taste from México!
+So if you are craving for Traditional Mexican menu items such as Quesadillas, Grilled Meats, Real Tacos, Guacamole and a great Drinks menu, what are you waiting for ?
+We are located in the heart of downtown Vancouver  on Granville Street.', 'https://images.pexels.com/photos/7772198/pexels-photo-7772198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', '-123.124006', '49.278797'),
 
-(3, 1, 'Test map 2', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+(2, 1, 'La Taqueria Pinche Taco Shop', 'All our dishes are made from traditional recipes using local, organic and sustainable products when possible. From our classic tacos, quesadillas and burritos to our new additions; we express a little bit of Mexico, their history and unique flavours in every dish. La Taqueria food is vibrant, delicious, fresh and fun.', 'https://images.pexels.com/photos/2092897/pexels-photo-2092897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', '-123.112674', '49.263558'),
 
-(3, 2, 'Test map 3', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+(2, 1, 'Mi Casa Mexicana', 'Home made style mexican food', 'https://images.squarespace-cdn.com/content/v1/5c35239c697a982d0e21bcc2/1621570365263-WHCXQ895T0NOXCLUO8NT/IMG_8955.JPG?format=500w', '-123.1458856', '49.2678006'),
 
-(4, 2, 'Test map 4', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+(2, 1, 'Chancho Tortilleria', 'In Mexico, at the heart of every community is the tortilleria. Chancho brings that authentic, small-town feeling to Vancouvers Yaletown neighbourhood.', 'https://images.pexels.com/photos/7613562/pexels-photo-7613562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', '-123.124905', '49.2761708'),
 
-(5, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457');
+(2, 1, 'La Mezcaleria', 'Latin American small plates, creative entrees & cocktails in arty, industrial-chic surrounds.', 'https://images.pexels.com/photos/4958730/pexels-photo-4958730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', '-123.0694291', '49.2701295'),
+
+-- AB
+(3, 1, 'Town of Banff', 'Located in the south end of Banff National Park, this little town, surrounded by mountains, is one of the main tourist destinations in Alberta. It is devoted to the tourism industry with all kinds of accommodation options, interesting shops, and restaurants that range from casual to high end.', 'https://www.planetware.com/photos-large/CDN/canada-banff-town-street.jpg', '-115.5682504', '51.1777781'),
+
+(3, 1, 'Lake Louise', 'its shimmering turquoise green water and surrounding snow-capped mountains rising up to 3,000 meters high. Although the water is too cold for swimming, it is ideal for canoeing. At the western end of Lake Louise, Mount Victoria (3,469 meters), rises in majestic splendor.', 'https://www.planetware.com/photos-large/CDN/canada-banff-lake-louise-canoes.jpg', '-116.22896211178153', '51.4114544'),
+
+(3, 1, 'Icefields Parkway', 'The Icefields Parkway runs from Lake Louise to Jasper and is one of the most beautiful drives in Canada. This 230-kilometer stretch of highway leads past lakes, mountains, glaciers, and waterfalls, with stopping points for visitors to get out and experience the landscape. Numerous hiking trails along the way, most of which are day hikes, lead to scenic lookouts over surrounding glaciers or lakes.', 'https://www.planetware.com/wpimages/2021/07/canada-banff-national-park-attractions-things-to-do-icefields-parkway-bow-lake.jpg', '-118.0529505', '52.8190712'),
+
+(3, 1, 'Moraine Lake', 'Moraine Lake in the Valley of the Ten Peaks. Like Lake Louise, this is another scenic setting with similar turquoise water and snow-capped peaks. It was for many years the location of the image on the reverse side of the old Canadian twenty dollar bill and is one of the most popular places to photograph in Canada.', 'https://www.planetware.com/photos-large/CDN/canada-banff-morraine-lake-2.jpg', '-116.1859853085267', '51.3215664'),
+
+(3, 1, 'Waterton Lakes National Park', 'The Waterton-Glacier International Peace Park in the Rocky Mountains straddles the border between Alberta and the U.S. state of Montana. On the Canadian side of the border is Waterton Lakes National Park, while on the American side is Glacier National Park.', 'https://www.planetware.com/photos-large/CDN/alberta-waterton-lakes-national-park.jpg', '-113.98100763190487', '49.101117349999996'),
+
+-- QUE
+(4, 2, 'The Canadian Museum of History', 'the countrys oldest museums, dating back to 1856. It also happens to be the most-visited in Canada, with 1.2 million annual visitors. So why do so many people stop by? The Gatineau museums four million items trace the history of Canada, and the world, starting 20,000 years ago. In the Grand Hall, discover the story, culture and beliefs of Canadas Pacific First Nations, in a beautiful setting overlooking the Ottawa River and Parliament Hill. ', 'https://d3d0lqu00lnqvz.cloudfront.net/media/media/0773437a-aa96-4c83-91a9-7490cb4a4fd7.jpg', '-75.70857565213802', '45.4298332'),
+
+(4, 2, 'The Montreal Botanical Garden', 'The expression stop and smell the flowers takes on a whole new meaning , one of the greatest in the world. With 22,000 plant species and cultivars living in its 10 greenhouses, 20 thematic gardens and a pavilion, you might have a tough time smelling them all, but why not dream big. Whether youre into lilacs, lotus flowers, orchids or succulents, or you just like looking at pretty flowers, theres a little something for everyone. An oasis within the city, only a few minutes from downtown. Its actually right beside the Olympic Stadium and Biodome, making for a great day trip.', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/04/ba/12/5d/montreal-botanical-gardens.jpg?w=1200&h=-1&s=1', '-73.56126486604046', '45.5595147'),
+
+(4, 2, 'Montmorency Falls Park', 'Many people are surprised to discover that a waterfall one and a half times higher than Niagara Falls sits just minutes from Quebec City. At 83m, the Montmorency Falls are a sight to behold, both from within the city and up close and personal in the Montmorency Falls Park (Parc de la Chute-Montmorency in French). Hang out near the base and feel the mist on your face, or ride the gondola up to the top for a spectacular view. More adventurous visitors might choose to hike one of the three Via Ferrata trails, stopping at viewpoints along the way for some Instagram-worthy shots, or even zip line across the falls. Of course, once you reach the top you can sit back and enjoy a great meal or a few celebratory drinks at the elegant Manoir Montmorency', 'https://images.squarespace-cdn.com/content/v1/591675241b10e3f7ad138acf/1500863785177-LVLD29MVYZ8B0Z9T1Y6K/Montmorency+Falls+from+Distance', '-71.1475263', '46.8905531'),
+
+(4, 2, 'Place Royale', 'Place Royale is the birthplace of Québec City, where Samuel de Champlain established the first habitation in 1608 and where there remains an outstanding collection of 17th- and 18th-century buildings that are a small sampling of Old Québec. The pretty stone church Notre-Dame des Victoires, dating to 1688, faces the square, along with modern tourist attractions like an outpost of the Musée de la Civilisation.', 'https://www.planetware.com/photos-large/CDN/canada-quebec-city-place-royale.jpg', '-73.55430308501505', '45.50322845'),
+
+(4, 2, 'Mont Tremblant', 'Ski resorts in the Canadian Laurentians are favorite winter destinations, and chief among them is Mont Tremblant - the highest peak in the Laurentians (960 meters), located about 150 kilometers north of Montréal. Good dining, entertainment, and ample accommodations define the resort community, which is housed in a quaint pedestrian village. The region is also a fall destination, when leaves turn autumn shades of orange, red, and gold.', 'https://officialmonttremblant.com/wp-content/uploads/2022/09/mont-tremblant-1.jpg', '-74.5891426', '46.11588'),
+
+-- TORONTO
+(5, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(5, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(5, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(5, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(5, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+-- NF
+(6, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(6, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(6, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(6, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457'),
+
+(6, 2, 'Test map 5', 'desc', 'https://picsum.photos/100/100', '-123.0998032', '49.2829457');
