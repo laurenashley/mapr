d1dd5f07dc5fb8a374a05bc8ed2edbb22e7b7f4e
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-01-13 17:43:09 -0700

    added padding for the map titles and fix a zoom on a map

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -6,5 +6,5 @@ VALUES
 (1, 'Best Mexican Food in Vancouver', '49.2578262', '-123.1941153', '11'),
 (1, 'Best Attractions Spots in Alberta', '51.0460954', '-114.065465', '6.8'),
 (2, 'Best Tourist Spots Around Montreal', '45.5031824', '-73.5698065', '7'),
-(2, 'Best place to eat Toronto', '43.6534817', '-79.3839347','12'),
+(2, 'Best place to Eat Toronto', '43.6534817', '-79.3839347','12'),
 (1, 'Best Airbnb in Niagara Falls', '43.1065603', '-79.0639039', '10');
